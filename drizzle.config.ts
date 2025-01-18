226237e69bef22ec7143b051ea65fb8f733357a7
BahaaZidan <gebnatorky@gmail.com>
2025-01-18 12:32:47 +0200

    "

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+import { defineConfig } from "drizzle-kit";
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
 export default defineConfig({
-	schema: './src/lib/server/db/schema.ts',
+	schema: "./src/lib/server/db/schema.ts",
 
 	dbCredentials: {
 		url: process.env.DATABASE_URL,
@@ -11,5 +11,5 @@ export default defineConfig({
 
 	verbose: true,
 	strict: true,
-	dialect: 'turso'
+	dialect: "turso"
 });
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,6 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
-export const user = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	age: integer('age')
+export const user = sqliteTable("user", {
+	id: integer("id").primaryKey(),
+	age: integer("age")
 });
